65d9d939db3f0d95db09ef855f11dfd594d6e028
Tim Hopper <tdhopper@gmail.com>
2016-01-09 14:56:45 -0500

    Post to facebook too

--- Makefile
+++ Makefile
@@ -2,5 +2,10 @@
 dependencies:
 	STATIC_DEPS=true pip install -Ur requirements.txt -t .
 
+prepare: dependencies
+	rm -f lambda_bundle.zip
+	zip lambda_bundle *
+	git clean --force -d --exclude *.zip
+
 clean:
 	git clean -fd
--- tweet.py
+++ tweet.py
@@ -7,23 +7,41 @@ import json
 r = requests.request("GET", "http://opc.org/today.html?target=archive")
 
 
+def update_facebook(title, url):
+    import facebook
+    with open('facebook.txt', 'r') as f:
+        access_token = f.read().strip()
+    api = facebook.GraphAPI(access_token)
+
+    try:
+        api.put_wall_post("", attachment={"link": url, "name": title})
+    except facebook.GraphAPIError as e:
+        print "Error", e
+
+
 def get_today():
     for date, path, title in re.findall(r'<p>([A-Za-z]+ [0-9]+)<br /><a href="(.*?)">(.*?)</a></p>', r.text):
         if date == dt.datetime.now().strftime("%B %-d"):
-            return "{} http://www.opc.org{}".format(title, path)
+            return title, "http://www.opc.org{}".format(path)
 
 
-def tweet(event, context):
+def update_twitter(title, url):
     with open("twitter.json", "r") as f:
         credentials = json.load(f)
     t = tw.Api(**credentials)
     try:
-        status = get_today()
+        status = "{} {}".format(title, url)
         t.PostUpdate(status=status)
         return "Tweeted {}".format(status)
     except Exception as e:
         return e.message
 
 
+def update(event=None, context=None):
+    title, url = get_today()
+    update_facebook(title, url)
+    update_twitter(title, url)
+
+
 if __name__ == '__main__':
-    print tweet(None, None)
+    update()
